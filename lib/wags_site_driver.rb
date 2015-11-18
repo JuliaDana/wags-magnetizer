f8dc7c37bb0cbb9b6eb647aabc00a111b2bbd04e
Julia Dana <juliadana@users.noreply.github.com>
2015-11-18 14:03:28 -0500

    Refactored loading main page of wags with poltergeist

--- lib/wags_site_driver.rb
+++ lib/wags_site_driver.rb
@@ -1,20 +1,33 @@
 require 'capybara/poltergeist'
-Capybara.javascript_driver = :poltergeist
+class WagsSiteDriver
+  include Capybara::DSL
 
-Capybara.configure do |config|
-  config.run_server = false
-  config.current_driver = :poltergeist
-  config.app = "fake app name"
-  config.app_host = "http://www.cs.appstate.edu/wags/"
+  WAGS_URL = "http://www.cs.appstate.edu/wags/"
+  PATH_TO_PHANTOMJS = File.expand_path("../../etc/phantomjs/bin/phantomjs", 
+    __FILE__)
+  
+  def initialize
+    Capybara.javascript_driver = :poltergeist
+
+
+    Capybara.configure do |config|
+      config.run_server = false
+      config.current_driver = :poltergeist
+      config.app_host = WAGS_URL
+    end
+
+    Capybara.register_driver :poltergeist do |app|
+      options = {}
+      options[:phantomjs] = PATH_TO_PHANTOMJS
+      Capybara::Poltergeist::Driver.new(app, options)
+    end
+  end
+
+  def test
+    visit("")
+    puts page.body
+  end
 end
 
-Capybara.register_driver :poltergeist do |app|
-  options = {}
-  options[:phantomjs] = "/home/julia/Projects/wags-magnet-generation/magnetizer/etc/phantomjs/bin/phantomjs"
-  Capybara::Poltergeist::Driver.new(app, options)
-end
-
-include Capybara::DSL
-visit("index")
-puts page.body
+WagsSiteDriver.new.test
 
