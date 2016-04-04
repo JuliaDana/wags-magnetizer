be8053c73503407ad89cef1a8a7496af10781795
Julia Dana <juliadana@users.noreply.github.com>
2016-04-04 17:21:46 -0400

    Loading Java information from configuration

--- lib/magnetizer/magnet_emitter_generator.rb
+++ lib/magnetizer/magnet_emitter_generator.rb
@@ -3,10 +3,10 @@ require_relative "magnet_emitter_base.rb"
 class MagnetEmitterGenerator
   def self.generate language = "Java"
     parent_class = eval "Java::#{language.downcase}_parser.#{language}BaseListener"
+    parser_class = eval  "Java::#{language.downcase}_parser.#{language}Parser"
     generating_class_name = "Generated#{language}MagnetEmitter"
 
-    languages = ["Java", "Python3"]
-    unless languages.include? language
+    unless LANGUAGES.keys.include? language
         raise UnsupportedLanguageError, "#{language} not supported by this #{self.name}"
     end
 
@@ -17,21 +17,16 @@ class MagnetEmitterGenerator
 
       case language
       when "Java"
+        language_info = LANGUAGES["Java"]
         emitter_class.class_eval do
-          type1 = ["PackageDeclaration", "ImportDeclaration"]
+          all_types = language_info["preamble_type"] +
+            language_info["class_type"] +
+            language_info["in_block_type"]
 
-          type2 = ["TypeDeclaration"]
-
-          type3 = ["ClassBodyDeclaration"]
-
-          type4 = ["BlockStatement"]
-
-          type2_and_3_and_4 = type1 + type2 + type3 + type4
-
-          type2_and_3_and_4.each do |node|
+          all_types.each do |node|
             define_method "enter#{node}" do |ctx|
               # TODO: not really needed for simple statements
-              if type4.include?(node) || type3.include?(node)
+              if language_info["in_block_type"].include? node
                 @exclusionIntervalsStack.last << ctx.getSourceInterval
               end
 
@@ -44,18 +39,26 @@ class MagnetEmitterGenerator
               # by other magnets. There should be drop zones at all excluded intervals.
               m.contents += getTextWithWhitespace ctx, @exclusionIntervalsStack.last
 
-              if type2.include? node
-                @classMagnets << m
-              elsif type3.include? node
-                if ctxHasChildType(ctx, Java::java_parser.JavaParser::MethodDeclarationContext, 2)
-                  @methodMagnets << m
-                else
-                  #raise "ClassBodyDeclaration node found that is not a method"
-                  @statementMagnets << m
+              used_override = false
+              if (o = language_info["overrides"]) && !o.empty?
+                o.each do |override|
+                  if override["name"] == node
+                    if ctxHasChildType(ctx,
+                        eval("#{parser_class}::#{override["child_node_type"]}Context"),
+                        override["child_level"])
+                      instance_variable_get("@#{override["location"]}Magnets") << m
+                      used_override = true
+                    end
+                  end
                 end
-              elsif type1.include? node
+              end
+              if used_override
+                # Do nothing
+              elsif language_info["class_type"].include? node
+                @classMagnets << m
+              elsif language_info["preamble_type"].include? node
                 @preambleMagnets << m
-              elsif type4.include? node
+              elsif language_info["in_block_type"].include? node
                 @statementMagnets << m
               end
               @exclusionIntervalsStack.pop
@@ -94,10 +97,12 @@ class MagnetEmitterGenerator
               if type2.include? node
                 @classMagnets << m
               elsif type3.include? node
-                if ctxHasChildType(ctx, Java::python3_parser.Python3Parser::ClassdefContext, 1)
+                if ctxHasChildType(ctx, 
+                    Java::python3_parser.Python3Parser::ClassdefContext, 1)
                   #@exclusionIntervalsStack.last << ctx.getSourceInterval
                   @classMagnets << m
-                elsif ctxHasChildType(ctx, Java::python3_parser.Python3Parser::FuncdefContext, 1)
+                elsif ctxHasChildType(ctx, 
+                    Java::python3_parser.Python3Parser::FuncdefContext, 1)
                   @methodMagnets << m
                 else
                   raise "compound statement node found that is not a method"
