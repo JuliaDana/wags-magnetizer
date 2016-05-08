a1dcf0be4a28c5715c0be3b9099671d68706d491
Julia Dana <juliadana@users.noreply.github.com>
2016-05-07 21:34:53 -0400

    More documentation

---
+++ doc/code/Hello.java
@@ -0,0 +1,15 @@
+/**
+ * Sample Javadoc comment
+ * @author Julia Dana
+ */
+public class Hello {
+  public String helloWorld() {
+    return "Hello World";
+  }
+
+  /* Sample multiline-style comment. */
+  public static void main(String[] args) {
+    // Sample singleline-style comment
+    System.out.println("Hello");
+  }
+}
